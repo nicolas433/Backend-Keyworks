1dcab7cc422ec3681c682e9131f4eebce9dd1f92
nicolas433 <ngrisoste@gmail.com>
2022-09-05 10:42:09 -0300

    feat: relacionamento entre times e usuários

---
+++ database/migrations/2022_09_04_185318_create_team_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_user', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('team_id')->unsigned();
+            $table->foreign('team_id')->references('id')->on('teams');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_user');
+    }
+};
